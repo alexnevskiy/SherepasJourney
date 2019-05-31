454dbfebb24eb5a3b8b5a449e0a4321bf5e400ab
Alex_Nevskiy <kam-2000@mail.ru>
2019-06-01 00:35:08 +0300

    Добавил скин для кнопок и изменён задний фон главного меню

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -34,6 +34,19 @@ public class Main extends Application {
         }
     }
 
+    String buttonStyle = "-fx-padding: 8 15 15 15;\n" +
+            "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
+            "    -fx-background-radius: 8;\n" +
+            "    -fx-background-color: \n" +
+            "        linear-gradient(from 0% 93% to 0% 100%, #13A32B 0%, #105C19 100%),\n" +
+            "        #469D24,\n" +
+            "        #64D83A,\n" +
+            "        radial-gradient(center 50% 50%, radius 100%, #3CAA3C, #1FFF20);\n" +
+            "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
+            "    -fx-font-weight: bold;\n" +
+            "    -fx-text-fill: #395306;" +
+            "    -fx-font-size: 4.0em;";
+
 
     public Main() {
     }
@@ -51,14 +64,19 @@ public class Main extends Application {
         exitButton = new Button("Выход");
         backButton = new Button("Назад");
 
+        playButton.setStyle(buttonStyle);
+        helpButton.setStyle(buttonStyle);
+        exitButton.setStyle(buttonStyle);
+        backButton.setStyle(buttonStyle);
+
         primaryStage.setOnCloseRequest(e -> System.exit(0));  //  Закрывается окно при нажатии на крестик
 
-        StackPane.setMargin(playButton, new Insets(0, 0, 300, 0));
-        StackPane.setMargin(helpButton, new Insets(0, 0, 0, 0));
-        StackPane.setMargin(exitButton, new Insets(300, 0, 0, 0));
-        StackPane.setMargin(backButton, new Insets(600, 0, 0, 1200));
+        StackPane.setMargin(playButton, new Insets(0, 800, 300, 0));
+        StackPane.setMargin(helpButton, new Insets(0, 800, 0, 0));
+        StackPane.setMargin(exitButton, new Insets(300, 800, 0, 0));
+        StackPane.setMargin(backButton, new Insets(600, 0, 0, 1080));
 
-        Image background = new Image(new FileInputStream("./images/BackgroundMenu.jpg"), 1280.0, 720.0, true, true);
+        Image background = new Image(new FileInputStream("./images/BackgroundMenu2.jpg"), 1280.0, 720.0, true, true);
         Image backgroundGame = new Image(new FileInputStream("./images/BackgroundGame.jpg"), 1280.0, 720.0, true, true);
         ImageView backgroundMenu = new ImageView(background);
         ImageView backgroundHelpMenu = new ImageView(background);
@@ -69,6 +87,7 @@ public class Main extends Application {
         gameLayout = new Pane(backgroundGameView);
 
         Scene mainScene = new Scene(layout, 1280, 720);
+//        mainScene.getStylesheets().add((getClass().getResource("/src/Main/css/styles.css")).toExternalForm());
         primaryStage.setScene(mainScene);
         primaryStage.show();
         Scene helpScene = new Scene(helpLayout, 1280, 720);
